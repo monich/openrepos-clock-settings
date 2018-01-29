073ca733558d14a64fac37210c68248cd5f03407
A <hucul@o2.pl>
2018-01-29 11:54:30 +0100

    Polish translation

---
+++ translations/openrepos-clock-settings-pl.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name></name>
+    <message id="settings_clock-he-clock">
+        <source>Clock</source>
+        <extracomment>Clock settings page header</extracomment>
+        <translation>Zegar</translation>
+    </message>
+    <message id="settings_clock-la-min">
+        <source>min</source>
+        <extracomment>Minutes (shortened)</extracomment>
+        <translation>min</translation>
+    </message>
+    <message id="settings_clock-la-snooze-interval">
+        <source>Snooze interval</source>
+        <extracomment>Slider label</extracomment>
+        <translation>Interwał drzemki</translation>
+    </message>
+    <message id="settings_clock-la-alarm-volume">
+        <source>Alarm volume</source>
+        <extracomment>Slider label</extracomment>
+        <translation>Głośność alarmu</translation>
+    </message>
+</context>
+</TS>

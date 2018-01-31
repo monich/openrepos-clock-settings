87df87f59d8575c44a9086cb747adbecccf684f7
p4moedo <35430331+p4moedo@users.noreply.github.com>
2018-01-31 22:25:57 +0100

    Create openrepos-clock-settings-es.ts
    Spanish translation

---
+++ translations/openrepos-clock-settings-es.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es">
+<context>
+    <name></name>
+    <message id="settings_clock-he-clock">
+        <source>Clock</source>
+        <extracomment>Clock settings page header</extracomment>
+        <translation>Reloj</translation>
+    </message>
+    <message id="settings_clock-la-min">
+        <source>min</source>
+        <extracomment>Minutes (shortened)</extracomment>
+        <translation>min</translation>
+    </message>
+    <message id="settings_clock-la-snooze-interval">
+        <source>Snooze interval</source>
+        <extracomment>Slider label</extracomment>
+        <translation>Intervalo de repetición</translation>
+    </message>
+    <message id="settings_clock-la-alarm-volume">
+        <source>Alarm volume</source>
+        <extracomment>Slider label</extracomment>
+        <translation>Volumen de la alarma</translation>
+    </message>
+</context>
+</TS>

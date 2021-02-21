48f014f286a6b8a0bc3097d3e49b84a4db5826dc
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2021-02-21 23:20:44 +0800

    Update openrepos-clock-settings-zh_CN.ts

--- translations/openrepos-clock-settings-zh_CN.ts
+++ translations/openrepos-clock-settings-zh_CN.ts
@@ -6,7 +6,7 @@
     <message id="openrepos_alert_settings-he-title">
         <source>Alerts</source>
         <extracomment>Page header and settings menu entry</extracomment>
-        <translation type="unfinished">闹钟</translation>
+        <translation>闹钟</translation>
     </message>
     <message id="openrepos_alert_settings-se-clock">
         <source>Clock</source>
@@ -16,7 +16,7 @@
     <message id="openrepos_alert_settings-se-calendar">
         <source>Calendar</source>
         <extracomment>Calendar settings section header</extracomment>
-        <translation type="unfinished">日历</translation>
+        <translation>日历</translation>
     </message>
     <message id="openrepos_alert_settings-la-min">
         <source>min</source>
@@ -26,32 +26,32 @@
     <message id="openrepos_alert_settings-la-clock_snooze">
         <source>Snooze interval</source>
         <extracomment>Slider label (clock snooze)</extracomment>
-        <translation type="unfinished">贪睡设置间隔</translation>
+        <translation>贪睡间隔时长</translation>
     </message>
     <message id="openrepos_alert_settings-la-clock_volume">
         <source>Alarm volume</source>
         <extracomment>Slider label (clock alarm)</extracomment>
-        <translation type="unfinished">闹钟音量</translation>
+        <translation>闹钟音量</translation>
     </message>
     <message id="openrepos_alert_settings-la-calendar_snooze">
         <source>Repeat interval</source>
         <extracomment>Slider label (repeat interval for calendar alerts)</extracomment>
-        <translation type="unfinished">重复间隔</translation>
+        <translation>重复间隔</translation>
     </message>
     <message id="openrepos_alert_settings-la-calendar_volume">
         <source>Alert volume</source>
         <extracomment>Slider label (calendar alert)</extracomment>
-        <translation type="unfinished">警报量</translation>
+        <translation>闹钟音量</translation>
     </message>
     <message id="openrepos_alert_settings-me-uninstall">
         <source>Uninstall</source>
         <extracomment>Pulley menu item</extracomment>
-        <translation>卸载</translation>
+        <translation>取消</translation>
     </message>
     <message id="openrepos_alert_settings-re-unisntalling">
         <source>Uninstalling Alerts settings</source>
         <extracomment>Remorse popup text</extracomment>
-        <translation type="unfinished">卸载警报设置</translation>
+        <translation>正在取消闹钟设置</translation>
     </message>
 </context>
 </TS>

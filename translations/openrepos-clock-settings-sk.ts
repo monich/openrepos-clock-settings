df9af3ff68bc44dbaff56d5f451cdb118e2b27eb
okruhliak <ladislav@hodas.sk>
2022-03-27 18:09:08 +0200

    Update openrepos-clock-settings-sk.ts

---
+++ translations/openrepos-clock-settings-sk.ts
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sk">
+<context>
+    <name></name>
+    <message id="openrepos_alert_settings-he-title">
+        <source>Alerts</source>
+        <extracomment>Page header and settings menu entry</extracomment>
+        <translation>Upozornenia</translation>
+    </message>
+    <message id="openrepos_alert_settings-se-clock">
+        <source>Clock</source>
+        <extracomment>Clock settings section header</extracomment>
+        <translation>Budík</translation>
+    </message>
+    <message id="openrepos_alert_settings-se-calendar">
+        <source>Calendar</source>
+        <extracomment>Calendar settings section header</extracomment>
+        <translation>Kalendár</translation>
+    </message>
+    <message id="openrepos_alert_settings-la-min">
+        <source>min</source>
+        <extracomment>Minutes (shortened)</extracomment>
+        <translation>min</translation>
+    </message>
+    <message id="openrepos_alert_settings-la-clock_snooze">
+        <source>Snooze interval</source>
+        <extracomment>Slider label (clock snooze)</extracomment>
+        <translation>Interval odloženia</translation>
+    </message>
+    <message id="openrepos_alert_settings-la-clock_volume">
+        <source>Alarm volume</source>
+        <extracomment>Slider label (clock alarm)</extracomment>
+        <translation>Hlasitosť budíka</translation>
+    </message>
+    <message id="openrepos_alert_settings-la-calendar_snooze">
+        <source>Repeat interval</source>
+        <extracomment>Slider label (repeat interval for calendar alerts)</extracomment>
+        <translation>Interval opakovania</translation>
+    </message>
+    <message id="openrepos_alert_settings-la-calendar_volume">
+        <source>Alert volume</source>
+        <extracomment>Slider label (calendar alert)</extracomment>
+        <translation>Hlasitosť upozornenia</translation>
+    </message>
+    <message id="openrepos_alert_settings-me-version">
+        <source>Version %1</source>
+        <extracomment>Pulley menu label</extracomment>
+        <translation>Verzia %1</translation>
+    </message>
+    <message id="openrepos_alert_settings-me-uninstall">
+        <source>Uninstall</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Odinštalovať</translation>
+    </message>
+    <message id="openrepos_alert_settings-re-unisntalling">
+        <source>Uninstalling Alerts settings</source>
+        <extracomment>Remorse popup text</extracomment>
+        <translation>Odinštalovať nastavenia</translation>
+    </message>
+</context>
+</TS>
